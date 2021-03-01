16c39ff24e8809d75dbef8d9f9783d22066452d7
eftihia_kiafa <eftihiakf@yahoo.gr>
2021-03-01 10:57:53 +0200

    Add files via upload

--- stockmng/views.py
+++ stockmng/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+     title='Welcome:This is the Home Page'
+     form ='Welcome:This is the Home Page'
+     context={
+     "title":title,
+     "test":form,
+     }
+     return render(request,'home.html',context)
+
+def list_item(request):
+     title='List of list_items'
+     queryset=Stock.objects.all()
+     context={
+     "title":title,
+     "queryset":queryset,
+     }
+     return render(request,'list_items.html',context)
