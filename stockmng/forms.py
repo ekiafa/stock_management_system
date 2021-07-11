0d09af69621fa8f19dbef54ea92a6be5c4fa3e02
ekiafa <kiafaeftychia@gmail.com>
2021-07-11 17:58:47 +0300

    delete some fields in stockcreateform

--- stockmng/forms.py
+++ stockmng/forms.py
@@ -5,8 +5,7 @@ from .models import Stock
 class StockCreateForm(forms.ModelForm):
    class Meta:
        model=Stock
-       fields=['symbol','name','sector','price','price_earnings','dividend_yield',	'earnings_share','fifty_two_week_low','fifty_two_week_high','market_cap','ebitda','price_sales','price_book','sec_fillings'
-]
+       fields=['symbol','name','sector','price','price_earnings']
 
 
 class StockCreateForm(forms.ModelForm):
