7fd947b7ca9494ad57e952b086f4a928473947a7
ekiafa <kiafaeftychia@gmail.com>
2021-07-11 17:24:15 +0300

    add search form in forms.py

--- stockmng/forms.py
+++ stockmng/forms.py
@@ -7,3 +7,9 @@ class StockCreateForm(forms.ModelForm):
        model=Stock
        fields=['symbol','name','sector','price','price_earnings','dividend_yield',	'earnings_share','fifty_two_week_low','fifty_two_week_high','market_cap','ebitda','price_sales','price_book','sec_fillings'
 ]
+
+
+class StockCreateForm(forms.ModelForm):
+   class Meta:
+       model=Stock
+       fields=["name","sector"]
